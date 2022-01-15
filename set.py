9e97e1aaf4be3b497d6cd3770368341395952812
DRASHHTIPATEL <90262225+DRASHHTIPATEL@users.noreply.github.com>
2022-01-15 22:06:54 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,49 @@
+'''STUDENT NAME:DRASHTI PATEL
+   STUDENT ID:20cs049
+ '''
+ 
+#1.Python program to add member(s) in a set and clear a set
+# Creating set 
+set = {'india','africa','australia','china','america'}
+#adding new zealand to the aboveset
+set.add('newzealand')
+#clearing the set created above
+set.clear()
+# 2.Write a Python program to remove an item from a set if it is present in the set.
+#create set named animals 
+animals = {'cow','dog','tiger','lion'}
+#print the given set 
+print(animals)
+#removing one attribute dog from the set
+animals.remove('dog')
+#print new set
+print(animals)
+
+
+# 3.Python program to create an intersection, Union, difference of sets.
+set1 = {'india','africa','australia','china','america'}
+set2 = {'cow','dog','tiger','lion'}
+print(set1| set2)
+print(set1 & set2)
+print(set1 - set2)
+
+ #4.Python program to find maximum and the minimum value in a set.
+ #create set named number
+number= {10,5,45,34,76,1}
+#print maximum number
+print(max(number))
+#print minimum number
+print(min(number))
+
+#5.Python program to find the most common elements and their counts from list, tuple, dictionary.
+
+Num1 = {
+      'num1': 15,
+      'num2': 15,
+      'num3': 15,
+Lst = {1, 2, 3, 2, 4, 5}
+def most_frequent(Lst) :
+    print(most_frequent(Lst))
+
+
+}
